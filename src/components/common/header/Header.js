6471125be9dfd26ce9b39803207cb4190955a5ff
Muhammad Zeeshan Mehdi <Zeeshanmehdi253@gmail.com>
2023-03-19 10:50:06 +0500

    third commit

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -2,23 +2,23 @@ import { MailOutline } from "@mui/icons-material";
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 import { Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import './header.css'
+import styles from './header.module.css'
 export default function Header() {
     return (
-        <Box className= 'header-box' >
-            <Grid container className="grid-container" >
+        <Box className= {styles.headerBox} >
+            <Grid container className={styles.gridContainer} >
             <Grid md={2} ></Grid>
             <Grid md={4} display={'flex'} justifyContent='space-evenly' flexDirection={'row'}  >
-                <Box className='content-box' >
+                <Box className={styles.contentBox} >
                 <Typography variant="body1">
-                <MailOutline className="header-icon" />
-                <a href="sd" >mhhasanul@gmail.com</a>
+                <MailOutline className={styles.headerIcon} />
+                <a href="sd" className={styles.links} >mhhasanul@gmail.com</a>
                 </Typography>
                 </Box>
-                <Box className='content-box' >
+                <Box className={styles.contentBox} >
                 <Typography variant="body1" >
-                <PhoneInTalkIcon className="header-icon" />
-                <a href="sfa" >(12345)67890</a>
+                <PhoneInTalkIcon className={styles.headerIcon} />
+                <a href="sfa" className={styles.links} >(12345)67890</a>
                 </Typography>
                 </Box>
             </Grid>
