50e062c9b3e360272c437df5a1b2c2f0a66e2e69
Muhammad Zeeshan Mehdi <Zeeshanmehdi253@gmail.com>
2023-03-19 10:19:56 +0500

    second commit

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,23 +1,24 @@
 import { MailOutline } from "@mui/icons-material";
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 import { Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import './header.module.css'
+import './header.css'
 export default function Header() {
     return (
-        <Box sx={{'backgroundColor':'var(--voilet)'}} >
-            <Grid container alignItems={'center'}height='44px'  >
+        <Box className= 'header-box' >
+            <Grid container className="grid-container" >
             <Grid md={2} ></Grid>
             <Grid md={4} display={'flex'} justifyContent='space-evenly' flexDirection={'row'}  >
-                <Box display={'flex'} alignItems='center' justifyContent={"center"} >
+                <Box className='content-box' >
                 <Typography variant="body1">
-                <MailOutline style={{'color':'white','verticalAlign':'middle','marginRight':'10px'}} />
-                <a style={{'color':'white','textDecoration':'none'}} href="sd" >mhhasanul@gmail.com</a>
+                <MailOutline className="header-icon" />
+                <a href="sd" >mhhasanul@gmail.com</a>
                 </Typography>
                 </Box>
-                <Box display={'flex'} alignItems='center' justifyContent={"center"} >
-                <Typography>
-                <MailOutline style={{'color':'white','verticalAlign':'middle','marginRight':'10px'}} />
-                <a style={{'color':'white','textDecoration':'none'}} href="sfa" >(12345)67890</a>
+                <Box className='content-box' >
+                <Typography variant="body1" >
+                <PhoneInTalkIcon className="header-icon" />
+                <a href="sfa" >(12345)67890</a>
                 </Typography>
                 </Box>
             </Grid>
