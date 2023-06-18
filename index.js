7c4027568516a0e08f282364d757a524683b5820
Zeeshan-Mehdi110 <Zeeshanmehdi253@gmail.com>
2023-06-18 09:20:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const siteController = require("./controllers/configurations");
 const brandController = require("./controllers/brands");
 
 // app.use
+
 app.use(express.json());
 app.use('/content', express.static('content/'));
 app.use(express.static(__dirname + `/client/build`))
