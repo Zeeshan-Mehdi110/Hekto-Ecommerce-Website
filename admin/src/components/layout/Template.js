1e2ac79fb7a01eb82bc1882727b52692a59771ce
Zeeshan-Mehdi110 <Zeeshanmehdi253@gmail.com>
2023-06-27 22:15:55 +0500

    Update Template.js

--- admin/src/components/layout/Template.js
+++ admin/src/components/layout/Template.js
@@ -1,11 +1,8 @@
-import React from 'react'
-import Header from './Header'
 import Sidebar from './Sidebar'
 
 function Template() {
   return (
     <>
-      {/* <Header /> */}
       <Sidebar />
     </>
   )
