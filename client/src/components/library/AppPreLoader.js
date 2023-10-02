a72fede34763814a1b722973f8721455a26a925c
Muhammad Zeeshan Mehdi <119069286+Zeeshan-Mehdi110@users.noreply.github.com>
2023-10-02 08:57:53 +0500

    Update AppPreLoader.js

--- client/src/components/library/AppPreLoader.js
+++ client/src/components/library/AppPreLoader.js
@@ -6,7 +6,7 @@ const AppPreLoader = ({ loader }) => {
     <Box display='flex' justifyContent='center' height='100%' alignItems='center'>
       <Box display={"flex"} textAlign={'center'} >
         {/* <CircularProgress /> */}
-        <img src={loader} alt="loadeing..." />
+        <img src={loader} alt="loadeing..." style={{ width: '114px', height: '114px' }} />
       </Box>
     </Box>
   )
